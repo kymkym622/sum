40b545e2fb08197222ae67df192155cd8f486457
Administrator <Administrator@305-15>
2016-04-27 15:42:14 +0900

    test문

---
+++ src/main/java/hansung/ky/test.java
@@ -0,0 +1,12 @@
+package hansung.ky;
+
+public class test {
+	private int a=0;
+	public void ttt(){
+		System.out.println("testtesttest");;
+	}
+	public int abc(){
+		return 5;
+	}
+
+}
---
+++ src/test/java/hansung/ky/testtest.java
@@ -0,0 +1,16 @@
+package hansung.ky;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import junit.framework.TestCase;
+
+public class testtest{
+	@Test
+	public void testttt(){
+		test aaa = new test();
+		assertEquals(5,aaa.abc());
+	}
+
+}
